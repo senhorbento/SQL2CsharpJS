79b039927bc02ea42f904cb0c4a0166bf4e14402
senhorbento <sbento.ti@gmail.com>
2022-07-02 15:14:14 -0300

    Refactor: Reorganizado funções

--- script.js
+++ script.js
@@ -1,79 +1,30 @@
-function Transpilar(){
-    var input = (document.getElementById("inputText").value).split(/\r?\n/); 
-    var classe;
-
-    document.getElementById('outputText').value = "";
-
-    input.forEach(function (item, indice, array){
-        classe = ChecarLinha(item);
-        if(classe != "")
-            document.getElementById('outputText').value += classe + "\n";
-    });
-}
-
-function ChecarLinha(item){
-    const _PROPRIEDADE_ = " { get; set; }";
+function ChecarEspecial(item){
     if(item.indexOf("CREATE TABLE") > -1){
         item = item.replace("CREATE TABLE", "public class"); 
         item = item.replace("(", "{"); 
-        return item;
+        return item + "\n";
     }
     if(item.indexOf(");") > -1){
         item = item.replace(");", "}");
-        return item;
-    } 
-    if(item.indexOf("TINYINT") > -1 || item.indexOf("tinyint") > -1){
-        var dividir = item.split(' ');
-        var variavel = RetornarVariavel(dividir);
-        item =  "    public sbyte " + variavel + _PROPRIEDADE_; 
-        return item; 
-    } 
-    if(item.indexOf("SMALLINT") > -1 || item.indexOf("smallint") > -1){
-        var dividir = item.split(' ');
-        var variavel = RetornarVariavel(dividir);
-        item =  "    public short " + variavel + _PROPRIEDADE_; 
-        return item; 
-    } 
-    if(item.indexOf("BIGINT") > -1 || item.indexOf("bigint") > -1){
-        var dividir = item.split(' ');
-        var variavel = RetornarVariavel(dividir);
-        item =  "    public long " + variavel + _PROPRIEDADE_; 
-        return item; 
-    }
-    if(PodeSerInt(item)){
-        var dividir = item.split(' ');
-        var variavel = RetornarVariavel(dividir);
-        item =  "    public int " + variavel + _PROPRIEDADE_; 
-        return item; 
-    }   
-    if(item.indexOf("FLOAT") > -1 || item.indexOf("float") > -1){
-        var dividir = item.split(' ');
-        var variavel = RetornarVariavel(dividir);
-        item =  "    public float " + variavel + _PROPRIEDADE_; 
-        return item; 
-    }
-    if(item.indexOf("DOUBLE") > -1 || item.indexOf("double") > -1){
-        var dividir = item.split(' ');
-        var variavel = RetornarVariavel(dividir);
-        item =  "    public double " + variavel + _PROPRIEDADE_;  
-        return item;
-    }  
-    if(PodeSerString(item)){
-        var dividir = item.split(' ');
-        var variavel = RetornarVariavel(dividir);
-        item =  "    public string " + variavel + _PROPRIEDADE_;  
-        return item;
-    }
-    if(PodeSerData(item)){
-        var dividir = item.split(' ');
-        var variavel = RetornarVariavel(dividir);
-        item =  "    public DateTime " + variavel + _PROPRIEDADE_;  
         return item;
     }
     return "";
 }
 
-function PodeSerInt(atributo){
+function RetornarPropriedade(dividir){
+    const _PROPRIEDADE_ = " { get; set; }";
+    var variavel, qtd = 0;
+
+    dividir.forEach(function (parte){
+        if(parte != '' && qtd == 0){
+            variavel = parte;
+            qtd++;
+        }
+    });
+    return variavel +  _PROPRIEDADE_ + "\n";
+}
+
+function ChecarInteiro(atributo){
     if(atributo.indexOf("MEDIUMINT") > -1 || atributo.indexOf("mediumint") > -1)
         return true;
     if(atributo.indexOf("INT") > -1 || atributo.indexOf("int") > -1)
@@ -81,7 +32,7 @@ function PodeSerInt(atributo){
     return false;
 }
 
-function PodeSerString(atributo){
+function ChecarString(atributo){
     if(atributo.indexOf("VARCHAR") > -1 || atributo.indexOf("varchar") > -1)
         return true;
     if(atributo.indexOf("CHAR") > -1 || atributo.indexOf("char") > -1)
@@ -89,7 +40,7 @@ function PodeSerString(atributo){
     return false;
 }
 
-function PodeSerData(atributo){
+function ChecarData(atributo){
     if(atributo.indexOf("DATETIME") > -1 || atributo.indexOf("datetime") > -1)
         return true;
     if(atributo.indexOf("DATE") > -1 || atributo.indexOf("date") > -1)
@@ -97,13 +48,57 @@ function PodeSerData(atributo){
     return false;
 }
 
-function RetornarVariavel(dividir){
-    var variavel, qtd =0;
-    dividir.forEach(function (parte, indice, array){
-        if(parte != '' && qtd == 0){
-            variavel = parte;
-            qtd++;
-        }
+function ChecarLinha(item){
+    var variavel = RetornarPropriedade(item.split(' '));
+    
+    if(item.indexOf("TINYINT") > -1 || item.indexOf("tinyint") > -1){
+        item =  "    public sbyte" + variavel; 
+        return item; 
+    } 
+    if(item.indexOf("SMALLINT") > -1 || item.indexOf("smallint") > -1){
+        item =  "    public short" + variavel; 
+        return item; 
+    } 
+    if(item.indexOf("BIGINT") > -1 || item.indexOf("bigint") > -1){
+        item =  "    public long" + variavel; 
+        return item; 
+    }
+    if(ChecarInteiro(item)){
+        item =  "    public int" + variavel; 
+        return item; 
+    }  
+    if(item.indexOf("FLOAT") > -1 || item.indexOf("float") > -1){
+        item =  "    public float" + variavel; 
+        return item; 
+    }
+    if(item.indexOf("DOUBLE") > -1 || item.indexOf("double") > -1){
+        item =  "    public double" + variavel;  
+        return item;
+    }  
+    if(ChecarString(item)){
+        item =  "    public string" + variavel;  
+        return item;
+    }
+    if(ChecarData(item)){
+        item =  "    public DateTime" + variavel;  
+        return item;
+    }
+    return "";
+}
+
+function Transpilar(){
+    var input = (document.getElementById("inputText").value).split(/\r?\n/); 
+    document.getElementById('outputText').value = "";
+
+    input.forEach(function(item){
+        var classe;
+        classe = ChecarEspecial(item);
+        if(classe != "")
+            document.getElementById('outputText').value += classe;
+        else{
+            classe = ChecarLinha(item);
+            if(classe != "")
+                document.getElementById('outputText').value += classe;
+        }  
     });
-    return variavel;
 }
