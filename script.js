4e8f9139963ffeeea197b0177e9dda8f7def5a9a
senhorbento <sbento.ti@gmail.com>
2022-10-24 17:36:13 -0300

    Refactor: Melhorada legibilidade

--- script.js
+++ script.js
@@ -1,74 +1,13 @@
 const _PROPRIEDADE_ = " { get; set; }";
 const VISIBILIDADE = "    public ";
 
-function ChecarCriacaoTabela(checar){
-    if(checar.toUpperCase().indexOf("CREATE TABLE") > -1){
-        checar = checar.replace(/CREATE TABLE/gi, "public class");  
-        return checar + "\n";
-    }
-    return checar;
-}
-
-function TrocarCaracterEspecial(checar){
-    if(checar.indexOf("(") > -1)  checar = checar.replace("(", "{");
-    if(checar.indexOf(");") > -1) checar = checar.replace(");", "}");
-    if(checar.indexOf(")") > -1)  checar = checar.replace(")", "}");
-    return checar;
-}
-
-function RetornarNomeTipo(dividir){
-    let variavel = [], qtd = 0;
-
-    dividir.forEach(function (parte){
-        if(parte != '' && qtd == 1){
-            variavel.push(parte);
-            qtd++;
-        }
-        if(parte != '' && qtd == 0){
-            variavel.push(parte +  _PROPRIEDADE_ + "\n");
-            qtd++;
-        }
-    });
-    return variavel;
-}
-
-function Inteiros(tipo){
-    if(tipo.indexOf("TINYINT") > -1)  return "sbyte ";
-    if(tipo.indexOf("SMALLINT") > -1) return "short "; 
-    if(tipo.indexOf("BIGINT") > -1)   return "long "; 
-    if(tipo.indexOf("INT") > -1)      return "int "; 
-    return false;
-}
-
-function OutrosTipos(tipo){
-    if(tipo.indexOf("FLOAT") > -1)   return "float "; 
-    if(tipo.indexOf("DOUBLE") > -1)  return "double ";  
-    if(tipo.indexOf("DECIMAL") > -1) return "decimal ";  
-    if(tipo.indexOf("CHAR") > -1)    return "string ";  
-    if(tipo.indexOf("DATE") > -1)    return "DateTime ";
-    return false;
-}
-
-function ChecarAtributo(atributo){
-    let nome = RetornarNomeTipo(atributo.split(' '));
-
-    if(nome[1] == undefined) return atributo;
-
-    let tipo = nome[1].toUpperCase();
-    nome = nome[0];
-
-    if(Inteiros(tipo)) return VISIBILIDADE + Inteiros(tipo) + nome;
-    if(OutrosTipos(tipo)) return VISIBILIDADE + OutrosTipos(tipo) + nome;
-    return atributo;
-}
-
 function LimparSaida(){
     document.getElementById('outputText').value = "";
 }
 
 function Transpilar(){
-    let input = (document.getElementById("inputText").value).split(/\r?\n/); 
     LimparSaida();
+    let input = (document.getElementById("inputText").value).split(/\r?\n/); 
     input.forEach(function(linha){
         let classe;
         let modificado = TrocarCaracterEspecial(linha);
@@ -82,4 +21,60 @@ function Transpilar(){
         else classe = modificado + "\n";
         if(modificado != "" && classe != -1) document.getElementById('outputText').value += classe;
     });
+}
+
+function TrocarCaracterEspecial(linha){
+    if(linha.indexOf("(") > -1)  linha = linha.replace("(", "{");
+    if(linha.indexOf(");") > -1) linha = linha.replace(");", "}");
+    if(linha.indexOf(")") > -1)  linha = linha.replace(")", "}");
+    return linha;
+}
+
+function ChecarAtributo(atributo){
+    let nome = RetornarNomeTipo(atributo.split(' '));
+
+    if(nome[1] == undefined) return atributo;
+
+    let tipo = nome[1].toUpperCase();
+    nome = nome[0];
+
+    if(ProcurarTipos(tipo)) return VISIBILIDADE + ProcurarTipos(tipo) + nome;
+    return atributo;
+}
+
+function RetornarNomeTipo(linha){
+    let variavel = [], qtd = 0;
+
+    linha.forEach(function (parte){
+        if(parte != '' && qtd == 1){
+            variavel.push(parte);
+            qtd++;
+        }
+        if(parte != '' && qtd == 0){
+            variavel.push(parte +  _PROPRIEDADE_ + "\n");
+            qtd++;
+        } 
+    });
+    return variavel;
+}
+
+function ProcurarTipos(linha){
+    if(linha.indexOf("TINYINT") > -1)  return "sbyte ";
+    if(linha.indexOf("SMALLINT") > -1) return "short "; 
+    if(linha.indexOf("BIGINT") > -1)   return "long "; 
+    if(linha.indexOf("INT") > -1)      return "int ";
+    if(linha.indexOf("FLOAT") > -1)    return "float "; 
+    if(linha.indexOf("DOUBLE") > -1)   return "double ";  
+    if(linha.indexOf("DECIMAL") > -1)  return "decimal ";  
+    if(linha.indexOf("CHAR") > -1)     return "string ";  
+    if(linha.indexOf("DATE") > -1)     return "DateTime ";
+    return false;
+}
+
+function ChecarCriacaoTabela(linha){
+    if(linha.toUpperCase().indexOf("CREATE TABLE") > -1){
+        linha = linha.replace(/CREATE TABLE/gi, "public class");  
+        return linha + "\n";
+    }
+    return linha;
 }
